cc76249f6e0c2f67d3ca8e718681d57cbe6cfc00
vladescualexandra <vladescualexandra18@stud.ase.ro>
2020-10-26 23:58:37 +0300

    Added WordActivity + FinishActivity. It's really bugged tho.

--- app/src/main/java/com/example/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.quiz_app;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.LabeledIntent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -13,12 +18,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView vladescu_alexandra_main_tv_welcome_text;
-    RadioGroup vladescu_alexandra_main_rg_language;
-    RadioButton vladescu_alexandra_main_rb_language_italian;
-    RadioButton vladescu_alexandra_main_rb_language_spanish;
-    RadioButton vladescu_alexandra_main_rb_language_french;
-    Button vladescu_alexandra_main_btn_go;
+    private static final String LANGUAGE_KEY = "language_key";
+    private TextView vladescu_alexandra_main_tv_welcome_text;
+    private RadioGroup vladescu_alexandra_main_rg_language;
+    private RadioButton vladescu_alexandra_main_rb_language_italian;
+    private RadioButton vladescu_alexandra_main_rb_language_spanish;
+    private RadioButton vladescu_alexandra_main_rb_language_french;
+    private Button vladescu_alexandra_main_btn_go;
+
+    private static int key = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,51 +35,54 @@ public class MainActivity extends AppCompatActivity {
 
         setComponents();
 
+        vladescu_alexandra_main_btn_go.setOnClickListener(startQuizEvent());
+
 
     }
 
-    public void setComponents() {
-         vladescu_alexandra_main_tv_welcome_text = findViewById(R.id.vladescu_alexandra_main_tv_welcome_text);
-         vladescu_alexandra_main_rg_language = findViewById(R.id.vladescu_alexandra_main_rg_language);
-         vladescu_alexandra_main_rb_language_italian = findViewById(R.id.vladescu_alexandra_main_rb_language_italian);
-         vladescu_alexandra_main_rb_language_spanish = findViewById(R.id.vladescu_alexandra_main_rb_language_spanish);
-         vladescu_alexandra_main_rb_language_french = findViewById(R.id.vladescu_alexandra_main_rb_language_french);
-         vladescu_alexandra_main_btn_go = findViewById(R.id.vladescu_alexandra_main_btn_go);
+    private View.OnClickListener startQuizEvent() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), WordActivity.class);
+                intent.putExtra("LANGUAGE_KEY", String.valueOf(key));
+                startActivity(intent);
+                finish();
+            }
+        };
     }
+
 
     @SuppressLint("NonConstantResourceId")
     public void onRadioButtonClicked(View view) {
         boolean checked = ((RadioButton) view).isChecked();
-        String language = "x";
-        
+
         switch (view.getId()) {
             case R.id.vladescu_alexandra_main_rb_language_italian:
                 if (checked) {
-                    // TODO set Italian
-                    language = "Italian"; // just debug
+                    key = 1;
                 }
                 break;
             case R.id.vladescu_alexandra_main_rb_language_spanish:
                 if (checked) {
-                    // TODO set Spanish
-                    language = "Spanish"; // just debug
+                    key = 2;
                 }
                 break;
             case R.id.vladescu_alexandra_main_rb_language_french:
                 if (checked) {
-                    // TODO set French
-                    language = "French"; // just debug
+                    key = 3;
                 }
                 break;
         }
-
-        
-        // TEST
-        Toast.makeText(getApplicationContext(),
-                language, 
-                Toast.LENGTH_LONG).show();
-        
     }
-    
-    
+
+    public void setComponents() {
+        vladescu_alexandra_main_tv_welcome_text = findViewById(R.id.vladescu_alexandra_main_tv_welcome_text);
+        vladescu_alexandra_main_rg_language = findViewById(R.id.vladescu_alexandra_main_rg_language);
+        vladescu_alexandra_main_rb_language_italian = findViewById(R.id.vladescu_alexandra_main_rb_language_italian);
+        vladescu_alexandra_main_rb_language_spanish = findViewById(R.id.vladescu_alexandra_main_rb_language_spanish);
+        vladescu_alexandra_main_rb_language_french = findViewById(R.id.vladescu_alexandra_main_rb_language_french);
+        vladescu_alexandra_main_btn_go = findViewById(R.id.vladescu_alexandra_main_btn_go);
+    }
 }
